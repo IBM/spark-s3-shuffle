e0cbc9ecb80d08469204f789b9780b3c7bcf37f7
Pascal Spörri <psp@zurich.ibm.com>
2023-08-15 12:36:00 +0200

    Use SBT Build Info plugin to populate version number.
    Signed-off-by: Pascal Spörri <psp@zurich.ibm.com>

--- .github/prepare-build.sh
+++ .github/prepare-build.sh
@@ -8,21 +8,6 @@ set -euo pipefail
 ROOT="$(cd "`dirname $0`/../" && pwd)"
 cd "${ROOT}"
 
-VERSION=$(git describe --tags || echo "vrev-$(git rev-parse --short HEAD)")
-VERSION="${VERSION:1}"
-echo "Version: ${VERSION}"
-
-# Change revision.
-FILES=(
-  "${ROOT}/build.sbt"
-  "${ROOT}/src/main/scala/org/apache/spark/shuffle/sort/S3ShuffleManager.scala"
-)
-
-for i in "${FILES[@]}"; do
-  echo "Replacing SNAPSHOT in ${i} with ${VERSION}"
-  sed -i "s/SNAPSHOT/${VERSION}/g" "${i}"
-done
-
 SCALA_VERSION=${SCALA_VERSION:-""}
 if [[ "${SCALA_VERSION:0:4}" == "2.13" ]]; then
   echo "Removing tests from build since ${SCALA_VERSION} is not supported!"
