e747aa78a8860d9cf4362c8dcf67061d78a38689
Pascal Spörri <psp@zurich.ibm.com>
2024-05-08 09:24:17 +0200

    Fix License Header for Scalafmt.
    Signed-off-by: Pascal Spörri <psp@zurich.ibm.com>

--- src/main/scala/org/apache/spark/shuffle/S3SingleSpillShuffleMapOutputWriter.scala
+++ src/main/scala/org/apache/spark/shuffle/S3SingleSpillShuffleMapOutputWriter.scala
@@ -1,7 +1,7 @@
-/**
- * Copyright 2023- IBM Inc. All rights reserved
- * SPDX-License-Identifier: Apache2.0
- */
+//
+// Copyright 2023- IBM Inc. All rights reserved
+// SPDX-License-Identifier: Apache 2.0
+//
 
 package org.apache.spark.shuffle
 
